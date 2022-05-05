78baefc119b840e8cc6e4de1f01dd726dcf80b9b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2022-05-05 12:50:20 -0400

    updte sessions_controller create and destroy methods

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
